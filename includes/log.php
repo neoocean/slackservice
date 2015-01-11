6b1b05278e7953d790abdd9284406b5babb339c6
Kim Woojin <wjkim@neoocean.net>
2015-01-11 11:29:38 +0900

    Add encoeText() to log.

--- includes/log.php
+++ includes/log.php
@@ -1,7 +1,10 @@
 <?php
 
+$log['command'] = encodeText($input['command']);
+$log['text'] = encodeText($input['text']);
+
 $sql = 'INSERT INTO logs (timestamp, token, team_id, channel_id, channel_name, user_id, user_name, command, text)
-		VALUES(\'' . time() . '\', \'' . $input['token'] . '\', \'' . $input['team_id'] . '\', \'' . $input['channel_id'] . '\', \'' . $input['channel_name'] . '\', \'' . $input['user_id'] . '\', \'' . $input['user_name'] . '\', \'' . $input['command'] . '\', \'' . $input['text'] . '\')';
+		VALUES(\'' . time() . '\', \'' . $input['token'] . '\', \'' . $input['team_id'] . '\', \'' . $input['channel_id'] . '\', \'' . $input['channel_name'] . '\', \'' . $input['user_id'] . '\', \'' . $input['user_name'] . '\', \'' . $log['command'] . '\', \'' . $log['text'] . '\')';
 if(!($result = $db->sql_query($sql)))
 {
 	die('쿼리에 실패했습니다: ' . $sql);
