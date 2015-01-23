3c13b772d65d1f0897eee88623938e8c65ff2a00
Kim Woojin <wjkim@neoocean.net>
2015-01-23 23:57:27 +0900

    New table structure.

--- tables.sql
+++ tables.sql
@@ -1,18 +1,36 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 4.3.3
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Jan 10, 2015 at 05:22 AM
--- Server version: 10.0.12-MariaDB
--- PHP Version: 5.2.17
+-- Host: slackservice.db
+-- Generation Time: Jan 23, 2015 at 02:56 PM
+-- Server version: 5.3.12-MariaDB
+-- PHP Version: 5.6.4-nfsn1
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
--- Database: `u578200698_slack`
+-- Database: `neoocean`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `channels`
+--
+
+CREATE TABLE IF NOT EXISTS `channels` (
+  `id` int(10) unsigned NOT NULL,
+  `channel_id` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `channel_name` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -22,9 +40,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `command_whattoeat_list` (
   `id` int(10) unsigned NOT NULL,
-  `name` text COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `name` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `links`
+--
+
+CREATE TABLE IF NOT EXISTS `links` (
+  `id` int(11) NOT NULL,
+  `link` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -40,6 +68,53 @@ CREATE TABLE IF NOT EXISTS `logs` (
   `channel_name` text COLLATE utf8_unicode_ci,
   `user_id` text COLLATE utf8_unicode_ci,
   `user_name` text COLLATE utf8_unicode_ci,
-  `command` text COLLATE utf8_unicode_ci,
-  `text` text COLLATE utf8_unicode_ci
+  `command` text CHARACTER SET utf8,
+  `text` text CHARACTER SET utf8
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `name` text COLLATE utf8_unicode_ci NOT NULL,
+  `realname` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `channels`
+--
+ALTER TABLE `channels`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `command_whattoeat_list`
+--
+ALTER TABLE `command_whattoeat_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `channels`
+--
+ALTER TABLE `channels`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
